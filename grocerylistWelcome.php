9fae0be88dcc99f651d777bb6ec64b192a6db5a2
bj9dorf <bj9dorf@gmail.com>
2015-02-08 01:11:41 -0700

    Add localhost vs openshift check in code

--- grocerylistWelcome.php
+++ grocerylistWelcome.php
@@ -8,11 +8,32 @@
 <h1>Welcome</h1>
 <p>Please Login</p>
 	<?php 
+
 		try 
 		{
-			$user = "bruce";
-			$password = "brucepass";
-			$db   = new PDO("mysql:host=localhost;dbname=shoppingList", $user, $password);
+			$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+			$dbName = "shoppingList";
+			if ($openShiftVar === null || $openShiftVar == "")
+			{
+			     // Not in the openshift environment
+			    $dbHost = "localhost";
+//   				$dbPort = "";
+     			$dbUser = "bruce"; //trying to get openshift to work
+				$dbPassword = "brucepass";
+				
+			     // …
+			}
+			else 
+			{
+			     // In the openshift environment 
+			    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+//				$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+				$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+				$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+			}
+
+			$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+//			echo "host:$dbHost dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 		}
 		catch (PDOException $ex)
 		{
