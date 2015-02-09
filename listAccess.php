c83cbe896e48b5fc3e98b176059ea3c6c916a751
bj9dorf <bj9dorf@gmail.com>
2015-02-09 02:37:27 -0700

    updated host on list acces

--- listAccess.php
+++ listAccess.php
@@ -29,7 +29,7 @@
  		$user = "bruce";
 		$pass = "brucepass";
 
-		$db = new PDO ("mysql:host=127.0.0.1;dbname=shoppingList",	$user, $pass);
+		$db = new PDO ("mysql:host=127.11.29.130;dbname=shoppingList",	$user, $pass);
 		$sql = "SELECT userId FROM user WHERE username=:name";
 		$stmt = $db->prepare($sql);
 		$stmt->bindValue(':name', $query, PDO::PARAM_STR);
